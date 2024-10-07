ee0e4dce4e643242de26dbbdbc0fbb5d5883e58b
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2024-10-07 20:00:41 +0200

    🔥 Retiramos en LAYOUT @/ alias e insertamos los ../ ya que no reconoce la navBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import { ControlDisplayProvider } from "@/context/ControlDisplay";
-import DisplayNavBar from "@/components/navBar/DisplayNavBar";
-
-/* const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-}); */
+import type { Metadata } from "next";
+import { ControlDisplayProvider } from "../context/ControlDisplay";
+import DisplayNavBar from "../components/navBar/DisplayNavBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ES">
-      <head>
-    
-      </head>
+      <head></head>
       <body>
         <ControlDisplayProvider>
           <DisplayNavBar />
