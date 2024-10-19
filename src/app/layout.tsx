3b81d519716d74bef53561931e135d7a80ea54dd
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2024-10-19 18:56:15 +0200

    ✅ 5 test, entrega con social imagen.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,17 @@ import DisplayNavBar from "@/components/nav/DisplayNavBar";
 import Footer from "@/components/footer/Footer";
 
 export const metadata: Metadata = {
-  title: "GTServicios - Gestión Transporte y Servicios a toda la Península",
+  title: "Gestión Transporte y Servicios a toda la Península",
   description:
     "Expertos en transporte y logística, con más de 40 años en el mercado, ofrecemos soluciones eficientes y confiables para tus necesidades de envío. Logística precisa, envíos eficientes.",
   keywords:
-    "gtservicios,gtservicios,gt servicios,servicio transporte mercancia,transporte mercancia,transporte de carga,transporte de mercancia en vallecas,transporte de mercancia en vallecas madrid,transporte de mercancia madrid,transporte de paqueteria,logistica de paqueteria,logistica y transporte,transporte de mercancia en españa,transporte de mercancia a toda españa,servicio de paqueteria,gestion paqueteria",
+    "gtservicios,gtservicios,gt servicios,servicio transporte mercancia,transporte mercancia,transporte de carga,transporte de mercancia en vallecas,transporte de mercancia en vallecas madrid,transporte de mercancia madrid,transporte de paqueteria,logistica de paqueteria,logistica y transporte,transporte de mercancia en españa,transporte de mercancia a toda españa,servicio de paqueteria,gestion paqueteria, vallecas transporte",
 };
 <head>
+  <title>
+    GTServicios - Gestión Transporte y Servicios a toda la Península
+  </title>
+
   <meta name="author" content="https://github.com/MoralesDaxz" />
   <meta name="copyright" content="© 2024 GTServicios" />
   <meta name="geo.region" content="ES" />
@@ -27,9 +31,31 @@ export const metadata: Metadata = {
   />
   <link rel="canonical" href="https://gtservicios.es" />
   <link rel="shortlink" href="https://gtservicios.es" />
-  <title>
-    GTServicios - Gestión Transporte y Servicios a toda la Península
-  </title>
+
+  <meta property="og:url" content="https://gtservicios.es" />
+  <meta property="og:type" content="website" />
+  <meta
+    property="og:title"
+    content="Gestión Transporte y Servicios a toda la Península"
+  />
+  <meta
+    property="og:description"
+    content="Expertos en transporte y logística, con más de 40 años en el mercado, ofrecemos soluciones eficientes y confiables para tus necesidades de envío. Logística precisa, envíos eficientes."
+  />
+  <meta property="og:image" content="https://i.ibb.co/dcdVcDT/gts.png" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="https://gtservicios.es" />
+  <meta property="twitter:url" content="https://gtservicios.es" />
+  <meta
+    name="twitter:title"
+    content="Gestión Transporte y Servicios a toda la Península"
+  />
+  <meta
+    name="twitter:description"
+    content="Expertos en transporte y logística, con más de 40 años en el mercado, ofrecemos soluciones eficientes y confiables para tus necesidades de envío. Logística precisa, envíos eficientes."
+  />
+  <meta property="og:image" content="https://i.ibb.co/dcdVcDT/gts.png" />
 </head>;
 export default function RootLayout({
   children,
